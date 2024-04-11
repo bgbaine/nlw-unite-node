9d37ac35099b2dde0eafb810ad3b10e1ead4ac96
Bernardo <bgbaine@gmail.com>
2024-04-11 17:32:56 -0300

    Optimizations

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
-/*  Pass in - REST API uses microframework Fastify, ORM Prisma and DB SQLite
+/*  
+
+    Pass in - REST API that uses microframework Fastify, ORM Prisma and DB SQLite
 
     HTTP Methods (GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS...)
     Request body (POST, PUT)
@@ -8,27 +10,38 @@
 
     Connect to databases
     Native driver / Query builders / Object Relational Mapping (ORM)
+
 */
 
-import fastify from "fastify";
-import { z } from "zod";
-import { PrismaClient } from "@prisma/client";
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
+
+// Creates application
 const app = fastify()
 
+// Constructs the Prisma Client
 const prisma = new PrismaClient({
-    log: ["query"]
+
+    // Makes every query create a log
+    log: ['query']
 })
 
+// POST in events route
 app.post('/events', async (request, reply) => {
+
+    // Creates validation checks
     const createEventSchema = z.object({
         title: z.string().min(4),
         details: z.string().nullable(),
         maximumAttendees: z.number().int().positive().nullable()
     })
     
+    // Validates sent data
     const data = createEventSchema.parse(request.body)
 
+    // Inserts data into 'event' database
     const event = await prisma.event.create({
         data: {
             title: data.title,
@@ -38,9 +51,11 @@ app.post('/events', async (request, reply) => {
         }
     })
 
+    // Returns status code 'Created' and id of created event (randomly generated)
     return reply.status(201).send({ eventId: event.id })
 })
 
+// Runs application
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
 })
