15972fd438f296421073f1e63a66dd0a4a05c710
Bernardo <bgbaine@gmail.com>
2024-07-24 21:19:58 -0300

    Aula 3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,5 @@
-/*
-    Title: prisma.ts
-    Description: Constructs Prisma Client
-*/
+import { PrismaClient } from "@prisma/client";
 
-import { PrismaClient } from '@prisma/client';
-
-
-// Constructs the Prisma Client
 export const prisma = new PrismaClient({
-
-    // Makes every query create a log
-    log: ['query']
+  log: ['query'],
 })
